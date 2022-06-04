ac948811acd08165b7c6a87507c4b4b192ef6464
DU-HWAN <lyder86@puroom.net>
2022-06-04 15:59:32 +0900

    delete Myheader

--- src/components/MyHeader.jsx
+++
@@ -1,9 +0,0 @@
-const myHeader = ({image}) => {
-	return (
-		<header className="py-8 px-8 max-w-sm mx-auto bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
-			<img className="bg-auto bg-no-repeat bg-center"  src={image}/>
-		</header>
-	);
-}
-
-export default myHeader;
